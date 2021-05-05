ca6c2a118a4e0be9ed83b4085efcfb7214a09285
pavan-kumar111 <83702938+pavan-kumar111@users.noreply.github.com>
2021-05-05 17:14:18 +0530

    import changes

--- index.js
+++ index.js
@@ -2,5 +2,7 @@
  * @format
  */
 
-import {NativeModules} from 'react-native';
-module.exports = NativeModules.CustomModule;
+// import {NativeModules} from 'react-native';
+// module.exports = NativeModules.CustomModule;
+import CustomModule from './CustomModule';
+export default CustomModule;
