763fd57723742f020a2ad53ea3120bc5ce358cf2
mohammedzihan <mohammedzihan001@gmail.com>
2021-05-10 11:08:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
 
-import React, {useEffect, useState} from 'react';
-import {Text} from 'react-native';
+// import React, {useEffect, useState} from 'react';
+// import {Text} from 'react-native';
 
-import {Button} from 'react-native';
-import CustomModule from './CustomModule';
-import {DeviceEventEmitter} from 'react-native';
+// import {Button} from 'react-native';
+// import CustomModule from './CustomModule';
+// import {DeviceEventEmitter} from 'react-native';
 
-const onDataAvailable = event => {
-  console.log(event);
-};
+// const onDataAvailable = event => {
+//   console.log(event);
+// };
 
-DeviceEventEmitter.addListener('onDataAvailable', onDataAvailable);
+// DeviceEventEmitter.addListener('onDataAvailable', onDataAvailable);
 
-function App() {
-  const callSdk = () => {
-    //   console.log('We will invoke the native module here!');
-    CustomModule.loadSdk();
-  };
+// function App() {
+//   const callSdk = () => {
+//     //   console.log('We will invoke the native module here!');
+//     CustomModule.loadSdk();
+//   };
 
-  return (
-    <Button
-      title="Click to invoke your native module!"
-      color="#841584"
-      onPress={() => callSdk()}
-    />
-  );
-}
+//   return (
+//     <Button
+//       title="Click to invoke your native module!"
+//       color="#841584"
+//       onPress={() => callSdk()}
+//     />
+//   );
+// }
 
-export default App;
+// export default App;
